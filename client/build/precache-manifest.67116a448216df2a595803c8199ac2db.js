991438f600155398e6a8761caf78fe03f24d5b44
vickel leung <>
2020-01-26 20:34:54 -0500

    no message

---
+++ client/build/precache-manifest.67116a448216df2a595803c8199ac2db.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a481429ba9860ce5f2872b515c31652c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a91ad64c8ccd2aae046",
+    "url": "/static/css/2.07b9f559.chunk.css"
+  },
+  {
+    "revision": "e8165e91b81eabf93d92",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2a91ad64c8ccd2aae046",
+    "url": "/static/js/2.46164162.chunk.js"
+  },
+  {
+    "revision": "d579785ec9ed1abf87e0e67e981d342d",
+    "url": "/static/js/2.46164162.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8165e91b81eabf93d92",
+    "url": "/static/js/main.506b2895.chunk.js"
+  },
+  {
+    "revision": "abb5dde4ed71d8390f0a",
+    "url": "/static/js/runtime-main.2ac92b4c.js"
+  },
+  {
+    "revision": "9b0a6fe79e2d63bbcc6412676e2be416",
+    "url": "/static/media/inforetrieve.9b0a6fe7.png"
+  },
+  {
+    "revision": "3fc3b7735c80d25708b285b0bfc79eb8",
+    "url": "/static/media/resume.3fc3b773.pdf"
+  },
+  {
+    "revision": "22b37099338dc15c7d7bfb28b411b18d",
+    "url": "/static/media/simonsay.22b37099.png"
+  },
+  {
+    "revision": "e559f09f9c5d9ae2a6b664b301d0ce98",
+    "url": "/static/media/software.e559f09f.png"
+  },
+  {
+    "revision": "1fc37423bc28b87baf4184682ba318fc",
+    "url": "/static/media/tictactoe.1fc37423.png"
+  }
+]);
